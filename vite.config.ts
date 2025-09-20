014be3b69fc22fc6435ab1d6d1b424d3db786b0f
oneronakforyou-a11y <oneronakforyou@gmail.com>
2025-09-20 08:33:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
     
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/my-app/', 
+      base: '/flowstate/',
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
